1e09f671cf75f61da3c117d059bc20834eceee99
Kamaljeet02 <kj98051@gmail.com>
2025-02-14 21:10:29 +0530

    sort 0,1,2

--- pep_dsa_code/dsa/DSA/arrays/array1.cpp
+++ pep_dsa_code/dsa/DSA/arrays/array1.cpp
@@ -1,5 +1,31 @@
 #include<iostream>
+#include<cmath>
+
 using namespace std;
+
+void sortColors(vector<int>& nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+
+            }
+        }
+
+
+}
+
 int main(){
     int  n;
     cin>> n;
