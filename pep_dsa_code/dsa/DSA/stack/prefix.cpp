582df8f9375ef0f31e27e28b838a8b5d3a3115ab
Kamaljeet02 <kj98051@gmail.com>
2025-02-25 00:11:55 +0530

    infix to postfix

--- pep_dsa_code/dsa/DSA/stack/prefix.cpp
+++ pep_dsa_code/dsa/DSA/stack/prefix.cpp
@@ -3,7 +3,7 @@
 #include<cmath>
 using namespace std;
 
-//polish notation.
+//polish notation. ...(prefix)
 int postfixEval(string s){
     stack<int> st;
 
@@ -80,6 +80,47 @@ int prefixEval(string s){
         }
     }
     return st.top();
+}
+
+//infix yo postfix
+int priority(char c){
+    if(c =='^') return 3;
+    else if(c=='*'|| c=='/') return 2;
+    else if(c=='+'||c=='-') return 1;
+    else return -1;
+}
+
+string infixToPostfix(string& s) {
+    // Your code here
+    stack<char> st;
+    string ans ="";
+    for(int i =0; i<s.size(); i++){
+        if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') ||(s[i]>='0' && s[i]<='9')){
+            ans +=s[i];
+        }
+        else if(s[i]=='('){
+            st.push(s[i]);
+        }
+        else if(s[i]==')'){
+            while( st.top()!='('){
+                ans +=st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+        else{
+            while(!st.empty()&& priority(s[i])<=priority(st.top())){
+                ans+= st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while(!st.empty()){
+        ans+=st.top();
+        st.pop();
+    }
+    return ans;
 }
 int main() {
     
