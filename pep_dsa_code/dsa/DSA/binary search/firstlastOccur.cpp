4817b32486d90ed2880b2c047d160de6f27a7ce7
Kamaljeet02 <kj98051@gmail.com>
2025-04-13 13:49:55 +0530

    search in sorted 1 corrected

--- pep_dsa_code/dsa/DSA/binary search/firstlastOccur.cpp
+++ pep_dsa_code/dsa/DSA/binary search/firstlastOccur.cpp
@@ -46,7 +46,33 @@ vector<int> searchRange(vector<int>& nums, int target) {
     return {first, last};
 }
 
-
+// SEARCH IN SORTED ARRAY bs
+int search(vector<int>& nums, int target) {
+    int low =0;
+    int high =nums.size()-1;
+    while(low<=high){
+        int mid =low +(high-low)/2;
+        if(nums[mid]==target) return mid;
+        //check which part is sorted
+        //Left part sorted
+        if(nums[low]<= nums[mid]){
+            // check target exist between left side
+            if(nums[low]<= target && target<= nums[mid]){
+                high =mid-1;
+            }
+            //element is in right side
+            else low= mid+1;
+        }
+        // RIGHT SORTED
+        else{
+            if(nums[mid]<= target && target <=nums[high]){
+                low= mid+1;
+            }
+            else high= mid-1;
+        }
+    }
+    return -1;
+}
 int main() {
     
     
