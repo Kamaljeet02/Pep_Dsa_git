b1983bbc66f806dff10ab0ec6d18e8841ed37004
Kamaljeet02 <kj98051@gmail.com>
2025-04-16 01:19:06 +0530

    find single non duplicate element

--- pep_dsa_code/dsa/DSA/binary search/firstlastOccur.cpp
+++ pep_dsa_code/dsa/DSA/binary search/firstlastOccur.cpp
@@ -131,6 +131,30 @@ int findMin(vector<int>& nums) {
     }
     return mini;
 }
+
+
+// Find single non duplicate element
+int singleNonDuplicate(vector<int>& nums) {
+    int low =1;
+    int high =nums.size()-2;
+    if(nums.size()==1) return nums[0];
+    if(nums[0]!=nums[1]) return nums[0];
+    if(nums[nums.size()-1] != nums[nums.size()-2]) return nums[nums.size()-1];
+
+    while(low<=high){
+        int mid =(low + high)/2;
+        if(nums[mid]!=nums[mid+1] && nums[mid]!= nums[mid-1]) return nums[mid];
+        // We are on left halff
+        //.   if mid is on odd position compare with previus odd.
+        if((mid%2==1 && nums[mid-1] ==nums[mid]) || (mid%2==0 && nums[mid]==nums[mid+1])){
+            low =mid+1; // eliminate left half 
+        }
+        else{
+            high =mid-1;
+        }
+    }
+    return -1;
+}
 int main() {
     
     
