05f3469851c66007288267f7ce2b07ad23f9ac52
Kamaljeet02 <kj98051@gmail.com>
2025-04-18 22:42:35 +0530

    Koko eating bananas

--- pep_dsa_code/dsa/DSA/binary search/firstlastOccur.cpp
+++ pep_dsa_code/dsa/DSA/binary search/firstlastOccur.cpp
@@ -179,6 +179,43 @@ int findPeakElement(vector<int>& nums) {
     }
     return -1;
 }
+
+// KOKO Earing Bananas
+int findMax(vector<int> piles){
+    int maxi =INT_MIN;
+    for(int i=0; i<piles.size(); i++){
+        maxi =max(maxi, piles[i]);
+    }
+    return maxi;
+}
+
+long long findHours(vector<int>& piles ,int perHour){
+    long long totalHours =0;
+    for(int i=0; i<piles.size();i++){
+        totalHours += ceil((double)piles[i]/ (double)perHour);
+    }
+    return totalHours;
+}
+
+int minEatingSpeed(vector<int>& piles, int h) {
+    int low =1;
+    int high = findMax(piles);
+    int ans;
+    while(low<=high){
+        int mid =(low+high)/2;
+        long long totalHours = findHours(piles, mid);
+        if(totalHours <= h){
+            ans= mid;
+            high =mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
+
 int main() {
     
     
