e16c87113016fcc023401af95dbb2b5045634ae8
Kamaljeet02 <kj98051@gmail.com>
2025-04-23 23:18:07 +0530

    Vertical order traveersal

--- pep_dsa_code/dsa/DSA/binary tree striver/tree1.cpp
+++ pep_dsa_code/dsa/DSA/binary tree striver/tree1.cpp
@@ -88,3 +88,32 @@ vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
     }
     return ans;
 }
+
+// Vertical Order Traversal
+vector<vector<int>> verticalTraversal(TreeNode* root) {
+    vector<vector<int>> ans;
+    if(root==NULL) return ans;
+    map<int, map<int, multiset<int>> >nodes;
+    queue< pair< TreeNode* , pair< int,int>> >todo;
+    todo.push({root,{0,0}});
+    while(!todo.empty()){
+        auto p =todo.front();
+        todo.pop();
+        TreeNode* node = p.first;
+        int v = p.second.first;
+        int l =p.second.second;
+        nodes[v][l].insert(node->val);
+        if(node->left) todo.push({node->left ,{v-1,l+1}});
+        if(node->right) todo.push({node->right ,{v+1,l+1}});
+        
+
+    }
+    for(auto p: nodes){
+        vector<int> col;
+        for(auto q: p.second){
+            col.insert(col.end(), q.second.begin(), q.second.end());
+        }
+        ans.push_back(col);
+    }
+    return ans;
+}
